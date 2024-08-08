62b095eedd9f6a9d215774aa4649944373615f81
unknown <alex_ohgren@hotmail.se>
2024-08-08 16:05:11 +0200

    bla bla

--- src/App.js
+++ src/App.js
@@ -18,7 +18,7 @@ function App() {
 
   return (
     <BrowserRouter>
-      <div style={{ display: "flex", overflow: "hidden"}}>
+      <div style={{ display: "flex", overflowY: "hidden"}}>
         <div>
           <Navbar homePage={homePage} problemPage={problemPage} consultantPage={consultantPage} aboutPage={aboutPage} 
           websitePage={websitePage} popupRef={popupRef}/>
